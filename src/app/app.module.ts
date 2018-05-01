fa8133d6f350eb341db786156a74251f467a971b
Rafael Díaz <palancana.bcn@gmail.com>
2018-05-01 12:00:58 +0200

    Tabs ordered and correctly named
    The tabs have been ordered (Favorites, Search, About) and show the icon only, not the name and icon.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { FavoritesPage } from '../pages/favorites/favorites';
-import { HomePage } from '../pages/home/home';
+import { SearchPage } from '../pages/search/search';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -16,7 +16,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     FavoritesPage,
-    HomePage,
+    SearchPage,
     TabsPage
   ],
   imports: [
@@ -28,7 +28,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     FavoritesPage,
-    HomePage,
+    SearchPage,
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { FavoritesPage } from '../favorites/favorites';
-import { HomePage } from '../home/home';
+import { SearchPage } from '../search/search';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = FavoritesPage;
+  tab1Root = FavoritesPage;
+  tab2Root = SearchPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
