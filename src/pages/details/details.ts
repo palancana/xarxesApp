f3b4911ac44446e8b36ea3f3b594739d0c5f15d6
Rafael Díaz <palancana.bcn@gmail.com>
2018-05-23 00:38:11 +0200

    added getEntityData(entity) function

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -6,12 +6,6 @@ import 'rxjs/add/operator/map';
 declare var require: any;
 var wdk = require("wikidata-sdk");
 
-/**
- * Generated class for the DetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -26,84 +20,141 @@ export class DetailsPage {
 
   id: any;
   label: any;
+  title: any;
   description: any;
+  image: any;
+  link: any;
   entity: any;
   personImage: any;
   personLink: any;
-  personCard: any;
+
+  searchUrl: any;
+  card: any;
+
+
 
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
 
+    this.searchUrl = {
+      ca: '',
+      en: '',
+      es: '',
+    }
+
+    this.card = {
+      person: {
+        id: '', //yes
+        label: '', //yes
+        description: '', //yes
+        image: '', //yes
+        link: '', //yes
+      },
+      historicalContext: {
+        id: '',
+        label: '',
+        description: '',
+        image: '',
+        link: ''
+      }
+    };
+
     this.person = navParams.get('data');
+    this.retrievePersonCard();
 
-    /*
-        //this.http.get('https://en.wikipedia.org/w/api.php?action=opensearch&search='+this.name+'&limit=1&format=json&origin=*').subscribe(
-   this.http.get('https://en.wikipedia.org/w/api.php?action=query&formatversion=2&prop=pageimages%7Cpageterms&titles='+this.person.name+'&format=json&origin=*').subscribe(
-    data => {
-          this.personCard = data.json();
-          console.log(this.personCard);
-      });
-    */
+    this.retrieveFamilyNameCard();
 
-    //Retrieves the name card information
-    const searchUrl = wdk.searchEntities({
+
+  }
+
+  ionViewDidLoad() {
+    
+  }
+
+  getEntityData(entity) {
+
+    //Gets label
+    if (entity.labels.hasOwnProperty('ca')) {
+      //Has Catalan label
+      this.card.person.label = entity.labels.ca;
+    } else {
+      //Does not have Catalan description
+      this.card.person.label = entity.labels.en;
+    }
+
+    //Gets descriptions
+    if (entity.descriptions.hasOwnProperty('ca')) {
+      //Has Catalan description
+      this.card.person.description = entity.descriptions.ca;
+    } else {
+      //Does not have Catalan description
+      this.card.person.description = entity.descriptions.en;
+    }
+
+    //Gets image
+    if (entity.claims.hasOwnProperty('P18')) {
+      //Has image property
+      this.card.person.image = wdk.getImageUrl(entity.claims.P18[0], 250);
+    } else {
+      //Does not have image property
+      this.card.person.image = './assets/imgs/default_card_image.png';
+    }
+
+    //Gets link
+    if (entity.sitelinks.hasOwnProperty('cawiki')) {
+      //Has Catalan Wikipedia link
+      this.card.person.link = entity.sitelinks.cawiki.url;
+    } else {
+      //Does not have Catalan Wikipedia link
+      this.card.person.link = '';
+    }
+
+  }
+
+  //Retrieves the person card information
+  retrievePersonCard() {
+
+    this.searchUrl.ca = wdk.searchEntities({
       search: this.person.name,
       limit: 1,
       language: 'ca'
     });
 
-    this.http.get(searchUrl).map(res => res.json()).subscribe(
+    this.http.get(this.searchUrl.ca).map(res => res.json()).subscribe(
       data => {
-          this.id = data.search[0].id;
-          this.label = data.search[0].label;
-          this.description = data.search[0].description;
-          
 
-          const getUrl = wdk.getEntities({
-            ids: this.id,
-            languages: ['ca'], // returns all languages if not specified
-            //props: ['P31'], // returns all data if not specified
-            format: 'json' // defaults to json
-          })
+        this.card.person.id = data.search[0].id;
 
+        const getUrl = wdk.getEntities({
+          ids: this.card.person.id,
+          languages: ['ca', 'es', 'en'], // returns all languages if not specified
+          props: ['claims', 'descriptions', 'labels', 'sitelinks'], // returns all data if not specified
+          format: 'json' // defaults to json
+        })
 
-          this.http.get(getUrl).map(res => res.json()).subscribe(
-            data => {
+        this.http.get(getUrl).map(res => res.json()).subscribe(
+          data => {
  
-              
+            var entity = wdk.simplify.entity(data.entities[this.card.person.id], {addUrl: true});
+            console.log(entity);
 
-              this.entity = data.entities[this.id];
-              if (this.entity.claims.hasOwnProperty('P18')) {
-                //Has image property
-                const simplifiedP18Claim = wdk.simplify.claim(this.entity.claims.P18[0]);
-                this.personImage = wdk.getImageUrl(simplifiedP18Claim, 250);
-              } else {
-                //Does not have image property
-                this.personImage = './assets/imgs/default_card_image.png';
-              }
-
-              if (this.entity.sitelinks.hasOwnProperty('cawiki')) {
-                //Has Catalan Wikipedia link
-                const simplifiedSiteLinks = wdk.simplify.sitelinks(this.entity.sitelinks, { addUrl: true });
-                this.personLink = simplifiedSiteLinks.cawiki.url;
-
-              } else {
-                //Does not have Catalan Wikipedia link
-                this.personLink = '';
-              }
-
-          
+            this.getEntityData(entity);
+            
             });
-
       });
+  }
+
+
+
+
+  retrieveFamilyNameCard() {
+    var spl = this.person.name.split(" ");
+    //console.log(spl[0]);
 
   }
 
-  ionViewDidLoad() {
-    //console.log('ionViewDidLoad DetailsPage');
-  }
+
 
   
 
