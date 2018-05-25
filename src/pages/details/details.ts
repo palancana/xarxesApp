2ad7a1fe70821e50b53557ad396289eb17d8d933
Rafael Díaz <palancana.bcn@gmail.com>
2018-05-25 11:57:17 +0200

    familyNameCard and personCard both work and have an optimization view

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -103,6 +103,7 @@ export class DetailsPage {
 
   //Retrieves the person card information
   retrievePersonCard() {
+    var t0 = performance.now();
 
     this.searchUrl.ca = wdk.searchEntities({
       search: this.person.name,
@@ -131,9 +132,12 @@ export class DetailsPage {
             
             });
       });
+      var t1 = performance.now();
+      console.log("Call to retrievePersonCard took " + (t1 - t0) + " milliseconds.");
   }
 
   retrieveFamilyNameCard() {
+    var t0 = performance.now();
     //Splits the name in an array using the space delimeter
     var familyNames = this.person.name.split(" ");
     //Takes out of the array the first element, which is the name, to leave only family names
@@ -145,27 +149,64 @@ export class DetailsPage {
       //Do something
     }
 
-    const familyName = 'Brown';
-    const sparqlq = `
-      SELECT ?item ?itemLabel ?articleEN ?articleES ?articleCA ?articleDE WHERE {
+    const familyName = 'Rodriguez'; //${familyName}
+    //Querys the needed data and has a fallback (ca => es => en) regarding itemLabel and itemDescription
+    const sparql = `
+      SELECT ?item ?itemLabel ?itemDescription ?image ?articleEN ?articleES ?articleCA ?articleDE WHERE {
         ?item wdt:P31 wd:Q101352.
-        ?item ?label "${familyName}"@en.
+        ?item ?label "${familyNames[0]}"@en.
+        OPTIONAL { ?item wdt:P18 ?image }
         OPTIONAL{?articleEN schema:about ?item .
         ?articleEN schema:isPartOf <https://en.wikipedia.org/>.}
         OPTIONAL{?articleES schema:about ?item .
         ?articleES schema:isPartOf <https://es.wikipedia.org/>.}
         OPTIONAL{?articleCA schema:about ?item .
         ?articleCA schema:isPartOf <https://ca.wikipedia.org/>.}
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "ca, es, en". }
       } LIMIT 1
-      `
-    const url = wdk.sparqlQuery(sparqlq);
+    `
 
-    this.http.get(this.searchUrl.ca).map(res => res.json()).subscribe(
+    const url = wdk.sparqlQuery(sparql);
+
+    this.http.get(url).map(res => res.json()).subscribe(
       data => {
         console.log(data);
-        //var entity = wdk.simplify.entity(data.search[0], {addUrl: true});
-        //console.log(entity);
+
+        //Retrieves the id
+        if (data.results.bindings[0].hasOwnProperty('item')) {
+          this.card.familyName.id = data.results.bindings[0].item.value;
+        } else {
+          this.card.familyName.id = 'none';
+        }
+
+        //Retrieves the label
+        if (data.results.bindings[0].hasOwnProperty('itemLabel')) {
+          this.card.familyName.label = data.results.bindings[0].itemLabel.value;
+        } else {
+          this.card.familyName.label = 'none';
+        }
+
+        //Retrieves the description
+        if (data.results.bindings[0].hasOwnProperty('itemDescription')) {
+          this.card.familyName.description = data.results.bindings[0].itemDescription.value;
+        } else {
+          this.card.familyName.description = 'none';
+        }
+
+        //Retrieves the image
+        if (data.results.bindings[0].hasOwnProperty('image')) {
+          this.card.familyName.image = (data.results.bindings[0].image.value + '?width=250');
+        } else {
+          this.card.familyName.image = './assets/imgs/default_card_image.png';
+        }
+
+        //Retrieves the link. Can make a fallback in the future.
+        if (data.results.bindings[0].hasOwnProperty('articleES')) {
+          this.card.familyName.link = data.results.bindings[0].articleES.value;
+        } else {
+          this.card.familyName.link = 'none';
+        }
+
       });
 
     /*
@@ -192,6 +233,8 @@ export class DetailsPage {
       });
   }
   */
+    var t1 = performance.now();
+    console.log("Call to retrieveFamilyNameCard took " + (t1 - t0) + " milliseconds.");
   }
 
 
